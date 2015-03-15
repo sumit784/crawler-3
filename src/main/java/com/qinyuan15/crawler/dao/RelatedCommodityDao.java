6866f8c3d9a3da1ea7744b7a89916804b120e163
qinyuan <qinyuan15@sina.com>
2015-03-15 14:16:58 +0800

    update

--- src/main/java/com/qinyuan15/crawler/dao/RelatedCommodityDao.java
+++ src/main/java/com/qinyuan15/crawler/dao/RelatedCommodityDao.java
@@ -1,8 +1,18 @@
 package com.qinyuan15.crawler.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Dao to deal with related commodity
  * Created by qinyuan on 15-3-15.
  */
 public class RelatedCommodityDao {
+    public List<Commodity> getInstances(Commodity commodity) {
+        if (commodity == null || commodity.getCategoryId() == null) {
+            return new ArrayList<>();
+        }
+
+        return CommodityDao.factory().setCategoryId(commodity.getCategoryId()).getInstances();
+    }
 }
---
+++ src/test/java/com/qinyuan15/crawler/dao/RelatedCommodityDaoTest.java
@@ -0,0 +1,18 @@
+package com.qinyuan15.crawler.dao;
+
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Test Related
+ * Created by qinyuan on 15-3-15.
+ */
+public class RelatedCommodityDaoTest {
+    @Test
+    public void testGetInstances() throws Exception {
+        Commodity commodity = new CommodityDao().getInstance(8);
+        List<Commodity> commodities = new RelatedCommodityDao().getInstances(commodity);
+        System.out.println(commodities.size());
+    }
+}
